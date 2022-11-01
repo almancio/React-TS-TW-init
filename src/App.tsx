2b2de309f7d6e1fca138f61f1ede8fa3f8d79417
Pablo Almancio <palmancio@Pablos-MacBook-Pro.local>
2022-11-02 00:07:57 +0100

    Feat: First commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+	return <div className="text-6xl text-clip">Probamos</div>
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{jsx,js,tsx,ts}", "index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
