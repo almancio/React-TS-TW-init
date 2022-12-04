945c364b84ca453b6f5b9b100f0f312a60a51ba9
Pablo Almancio <palmancio@po526629.generalitat.gva.es>
2022-12-04 14:31:56 +0100

    chore: :art: add new extension for vscode recommendation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import {
+  Dispatch,
+  MouseEventHandler,
+  ReactComponentElement,
+  SetStateAction,
+  useState
+} from 'react'
 function App() {
-  return <div className="text-6xl text-clip">Probamos</div>
+  const [count, setCount] = useState(0)
+  const increment: MouseEventHandler<HTMLButtonElement> = () =>
+    setCount(count + 1)
+  const decrement: MouseEventHandler<HTMLButtonElement> = () =>
+    setCount(count - 1)
+  const value: String = count > 0 ? 'positive' : 'non-positive'
+  return (
+    <div>
+      <p className="text-3xl text-clip text-red-500">
+        You clicked {count} times and is {value}
+      </p>
+      <button
+        className="p-3 max-w-sm mx-auto text-gray-700 bg-red-400 rounded-xl shadow-xl w-30 flex items-center space-x-4 hover:bg-sky-700 hover:text-blue-900 active:bg-black active:text-white transition-colors duration-300 ease-in-out"
+        onClick={increment}
+      >
+        Click me
+      </button>
+      <button onClick={decrement}>Click me</button>
+    </div>
+  )
 }
 
 export default App
